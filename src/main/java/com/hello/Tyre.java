35f9ebf56766ab3553c4ade9132fa464b93aa370
PJ <goldprajnawi@gmail.com>
2021-06-15 13:49:01 +0800

    constructor annotation

--- src/main/java/com/hello/Tyre.java
+++ src/main/java/com/hello/Tyre.java
@@ -4,10 +4,16 @@ public class Tyre {
     private String brand;
 
 
+    // this is for constructor injection
+    public Tyre(String brand) {
+        this.brand = brand;
+    }
+
     public String getBrand() {
         return this.brand;
     }
 
+      // this is for bean property
     public void setBrand(String brand) {
         this.brand = brand;
     }
